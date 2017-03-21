4bee9c155febbd7bb4b0f44fb8e42c57085d1734
Hendrik <hbelitz@ieee.org>
2017-03-21 18:47:51 +0100

    Still trying to create npm package

--- build/src/maybemonad.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function maybe<T>(val: T): Maybe<T>;
-export interface Maybe<T> {
-    map<U>(f: (x: T) => U): Maybe<U>;
-    flatMap<U>(f: (x: T) => Maybe<U>): Maybe<U>;
-    orElse(def: T): T;
-    unsafeLift(): T;
-    nothing(): boolean;
-}
